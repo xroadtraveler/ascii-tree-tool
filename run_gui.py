9686e362d9f933f8f1a36e72c7472d95ba804ca7
Travis Rillos <xroadtraveler@gmail.com>
2026-08-23 13:05:44 -0700

    Add repo-root launcher for PyInstaller entry point

---
+++ run_gui.py
@@ -0,0 +1,9 @@
+# [10000] Beacon intent: PyInstaller launcher for ascii-tree-tool
+
+from ascii_tree_tool.__main__ import main
+
+
+# [010] enforce script entry
+if __name__ == "__main__":
+    main()
+# [-----END [010]-----]
